a63b559d55e6438c2686341b2e1c46f3f0a0bbb3
AxelSolis93 <104176510+AxelSolis93@users.noreply.github.com>
2022-04-27 23:27:04 -0500

    Add files via upload

---
+++ LAB02_GRUPO_B_20213132_AXEL_SOLIS/Ejercicio 3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>//Axel Jhuan Solis Zamata
+using namespace std;
+int main()
+{	
+	cout<<"Los numeros multiplos de 5 comprendido entre 1 y 100 son ";
+	int i=1;
+	while(5*i!=100){
+		cout << 5*i << " ";
+		i++;
+	}
+	cout << endl;
+	cout << endl << endl;
+	system("pause");
+}
